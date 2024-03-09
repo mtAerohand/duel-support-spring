7e7cb61eb87f52d6d0811a98bcefbde0eaae7a9c
Takefumi Motoyama <mtaerohand@gmail.com>
2024-03-10 00:02:01 +0900

    init

---
+++ src/main/java/com/mtaerohand/duelsupportspring/DuelSupportSpringApplication.java
@@ -0,0 +1,13 @@
+package com.mtaerohand.duelsupportspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuelSupportSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuelSupportSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtaerohand/duelsupportspring/DuelSupportSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtaerohand.duelsupportspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuelSupportSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
