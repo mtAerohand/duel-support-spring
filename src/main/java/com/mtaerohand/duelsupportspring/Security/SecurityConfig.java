945661a4f6222dfcf11b653553b69749272fd813
Takefumi Motoyama <mtaerohand@gmail.com>
2024-03-10 23:21:48 +0900

    b: cors非対応 a: react appのorijinを許可する設定をSecurityFilterに追加

--- src/main/java/com/mtaerohand/duelsupportspring/Security/SecurityConfig.java
+++ src/main/java/com/mtaerohand/duelsupportspring/Security/SecurityConfig.java
@@ -1,11 +1,16 @@
 package com.mtaerohand.duelsupportspring.Security;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -13,8 +18,19 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(customizer -> customizer
-                .anyRequest().permitAll());
+        http.cors(customizer -> customizer.configurationSource(corsConfigurationSource()))
+                .authorizeHttpRequests(customizer -> customizer
+                        .anyRequest().permitAll());
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
