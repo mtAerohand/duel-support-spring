4f37974b6547ccf773425e3a427229ef3e83345a
Takefumi Motoyama <mtaerohand@gmail.com>
2024-04-09 22:57:54 +0900

    a: 必要ないエンドポイントは作成せずにdbを直接操作する方針で決定

--- src/main/java/com/mtaerohand/duelsupportspring/controller/DeckController/DeckController.java
+++ src/main/java/com/mtaerohand/duelsupportspring/controller/DeckController/DeckController.java
@@ -2,25 +2,16 @@ package com.mtaerohand.duelsupportspring.controller.DeckController;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mtaerohand.duelsupportspring.controller.DeckController.CreateDecksRequest.CreateDecksRequest;
-import com.mtaerohand.duelsupportspring.controller.DeckController.CreateDecksResponse.CreateDecksResponse;
-import com.mtaerohand.duelsupportspring.controller.DeckController.DeleteDecksRequest.DeleteDecksRequest;
 import com.mtaerohand.duelsupportspring.controller.DeckController.GetDecksResponse.GetDecksResponse;
-import com.mtaerohand.duelsupportspring.controller.DeckController.UpdateDecksRequest.UpdateDecksRequest;
 import com.mtaerohand.duelsupportspring.service.DeckService;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
 
 /**
  * デッキ情報コントローラ
@@ -65,12 +56,13 @@ public class DeckController {
      * @param req デッキ情報の一括作成リクエスト
      * @return デッキ情報の一括作成レスポンス
      */
-    @PostMapping("decks")
-    @ResponseStatus(HttpStatus.CREATED)
-    public CreateDecksResponse createDecks(@Valid @RequestBody CreateDecksRequest req) {
-        CreateDecksResponse res = deckService.createDecks(req);
-        return res;
-    }
+    // @PostMapping("decks")
+    // @ResponseStatus(HttpStatus.CREATED)
+    // public CreateDecksResponse createDecks(@Valid @RequestBody CreateDecksRequest
+    // req) {
+    // CreateDecksResponse res = deckService.createDecks(req);
+    // return res;
+    // }
 
     /**
      * デッキ情報の一括更新
@@ -78,19 +70,19 @@ public class DeckController {
      * @param req デッキ情報の一括更新リクエスト
      * @return デッキ情報の一括更新レスポンス
      */
-    @PutMapping("decks")
-    @ResponseStatus(HttpStatus.CREATED)
-    public void updateDecks(@Valid @RequestBody UpdateDecksRequest req) {
-        deckService.updateDecks(req);
-    }
+    // @PutMapping("decks")
+    // @ResponseStatus(HttpStatus.CREATED)
+    // public void updateDecks(@Valid @RequestBody UpdateDecksRequest req) {
+    // deckService.updateDecks(req);
+    // }
 
     /**
      * デッキ情報の一括削除
      * 
      */
-    @DeleteMapping("decks")
-    @ResponseStatus(HttpStatus.OK)
-    public void deleteDecks(@Valid @RequestBody DeleteDecksRequest req) {
-        deckService.deleteDecks(req);
-    }
+    // @DeleteMapping("decks")
+    // @ResponseStatus(HttpStatus.OK)
+    // public void deleteDecks(@Valid @RequestBody DeleteDecksRequest req) {
+    // deckService.deleteDecks(req);
+    // }
 }
--- src/main/java/com/mtaerohand/duelsupportspring/service/DeckService.java
+++ src/main/java/com/mtaerohand/duelsupportspring/service/DeckService.java
@@ -1,6 +1,5 @@
 package com.mtaerohand.duelsupportspring.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.modelmapper.ModelMapper;
@@ -9,14 +8,8 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.mtaerohand.duelsupportspring.controller.DeckController.GetDeckResponse;
-import com.mtaerohand.duelsupportspring.controller.DeckController.CreateDecksRequest.CreateDecksRequest;
-import com.mtaerohand.duelsupportspring.controller.DeckController.CreateDecksResponse.CreateDecksResponse;
-import com.mtaerohand.duelsupportspring.controller.DeckController.CreateDecksResponse.CreateDecksResponseDeck;
-import com.mtaerohand.duelsupportspring.controller.DeckController.DeleteDecksRequest.DeleteDecksRequest;
 import com.mtaerohand.duelsupportspring.controller.DeckController.GetDecksResponse.GetDecksResponse;
 import com.mtaerohand.duelsupportspring.controller.DeckController.GetDecksResponse.GetDecksResponseDeck;
-import com.mtaerohand.duelsupportspring.controller.DeckController.UpdateDecksRequest.UpdateDecksRequest;
-import com.mtaerohand.duelsupportspring.controller.DeckController.UpdateDecksRequest.UpdateDecksRequestDeck;
 import com.mtaerohand.duelsupportspring.repository.DeckRepository.Deck;
 import com.mtaerohand.duelsupportspring.repository.DeckRepository.DeckRepository;
 
@@ -73,24 +66,25 @@ public class DeckService {
      * @param req デッキ情報の一括作成リクエスト
      * @return デッキ情報の一括作成レスポンス
      */
-    public CreateDecksResponse createDecks(CreateDecksRequest req) {
-        List<Deck> decksForCreate = modelMapper.map(req.getDecks(), new TypeToken<List<Deck>>() {
-        }.getType());
-        // nullチェック
-        if (decksForCreate == null) {
-            return new CreateDecksResponse();
-        }
-        // リクエストデータをデッキテーブルの保存
-        List<Deck> decksCreated = deckRepository.saveAll(decksForCreate);
+    // public CreateDecksResponse createDecks(CreateDecksRequest req) {
+    // List<Deck> decksForCreate = modelMapper.map(req.getDecks(), new
+    // TypeToken<List<Deck>>() {
+    // }.getType());
+    // // nullチェック
+    // if (decksForCreate == null) {
+    // return new CreateDecksResponse();
+    // }
+    // // リクエストデータをデッキテーブルの保存
+    // List<Deck> decksCreated = deckRepository.saveAll(decksForCreate);
 
-        // レスポンスデータを生成し返却
-        List<CreateDecksResponseDeck> resDecks = modelMapper.map(decksCreated,
-                new TypeToken<List<CreateDecksResponseDeck>>() {
-                }.getType());
-        CreateDecksResponse res = new CreateDecksResponse();
-        res.setDecks(resDecks);
-        return res;
-    }
+    // // レスポンスデータを生成し返却
+    // List<CreateDecksResponseDeck> resDecks = modelMapper.map(decksCreated,
+    // new TypeToken<List<CreateDecksResponseDeck>>() {
+    // }.getType());
+    // CreateDecksResponse res = new CreateDecksResponse();
+    // res.setDecks(resDecks);
+    // return res;
+    // }
 
     /**
      * デッキ情報の一括更新
@@ -98,27 +92,27 @@ public class DeckService {
      * @param req デッキ情報の一括更新リクエスト
      * @return デッキ情報の一括更新レスポンス
      */
-    public void updateDecks(UpdateDecksRequest req) {
-        // 更新対象のレコードを取得し入力値をマッピング
-        List<Deck> decksForUpdate = new ArrayList<Deck>();
-        for (UpdateDecksRequestDeck deck : req.getDecks()) {
-            // TODO: エラーメッセージをまとめる
-            // TODO: 入力を丸ごとマッピングするのではなく、入力があった項目のみアップデートするほうがよさそう。
-            Deck deckForUpdate = deckRepository.findById(deck.getId())
-                    .orElseThrow(() -> new RuntimeException("指定されたIDのデッキが存在しません。"));
-            deckForUpdate = modelMapper.map(deck, Deck.class);
-            decksForUpdate.add(deckForUpdate);
-        }
-        // 対象レコードの更新処理
-        deckRepository.saveAll(decksForUpdate);
-    }
+    // public void updateDecks(UpdateDecksRequest req) {
+    // // 更新対象のレコードを取得し入力値をマッピング
+    // List<Deck> decksForUpdate = new ArrayList<Deck>();
+    // for (UpdateDecksRequestDeck deck : req.getDecks()) {
+    // // TODO: エラーメッセージをまとめる
+    // // TODO: 入力を丸ごとマッピングするのではなく、入力があった項目のみアップデートするほうがよさそう。
+    // Deck deckForUpdate = deckRepository.findById(deck.getId())
+    // .orElseThrow(() -> new RuntimeException("指定されたIDのデッキが存在しません。"));
+    // deckForUpdate = modelMapper.map(deck, Deck.class);
+    // decksForUpdate.add(deckForUpdate);
+    // }
+    // // 対象レコードの更新処理
+    // deckRepository.saveAll(decksForUpdate);
+    // }
 
     /**
      * デッキ情報の一括削除
      * 
      * @param req デッキ情報の一括削除リクエスト
      */
-    public void deleteDecks(DeleteDecksRequest req) {
-        deckRepository.deleteAllById(req.getDeckIds());
-    }
+    // public void deleteDecks(DeleteDecksRequest req) {
+    // deckRepository.deleteAllById(req.getDeckIds());
+    // }
 }
