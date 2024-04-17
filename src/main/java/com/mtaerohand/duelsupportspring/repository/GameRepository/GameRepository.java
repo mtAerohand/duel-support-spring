3bd36452fd0a8bdeb9081ca101bd4ae1a2219ef0
Takefumi Motoyama <mtaerohand@gmail.com>
2024-04-18 00:18:59 +0900

    a: ネイティブクエリがmysqlとポスグレで書き方が変わるためprod用のリポジトリの記述をGameRepositoryに追加

--- src/main/java/com/mtaerohand/duelsupportspring/repository/GameRepository/GameRepository.java
+++ src/main/java/com/mtaerohand/duelsupportspring/repository/GameRepository/GameRepository.java
@@ -10,20 +10,114 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+// /**
+//  * 試合情報リポジトリ(開発用)
+//  */
+// @Repository
+// public interface GameRepository extends JpaRepository<Game, Integer>, JpaSpecificationExecutor<Game> {
+//         // デッキ分布一覧情報の取得
+//         @Query("SELECT NEW com.mtaerohand.duelsupportspring.repository.GameRepository.DeckDistribution(g.myDeckId AS deckId, CAST(COUNT(g) / (SELECT COUNT(g2) FROM Game g2) AS FLOAT) AS ratio) "
+//                         + "FROM Game g WHERE g.modeId = :modeId AND g.modeDetailId = :modeDetailId "
+//                         + "GROUP BY g.myDeckId")
+//         List<DeckDistribution> getDeckDistributions(@Param("modeId") Integer modeId,
+//                         @Param("modeDetailId") Integer modeDetailId, Pageable pageable);
+
+//         // デッキ分布一覧情報の取得(時間指定あり)
+//         @Query("SELECT NEW com.mtaerohand.duelsupportspring.repository.GameRepository.DeckDistribution(g.myDeckId AS deckId, CAST(COUNT(g) / (SELECT COUNT(g2) FROM Game g2) AS FLOAT) AS ratio) "
+//                         + "FROM Game g WHERE g.modeId = :modeId AND g.modeDetailId = :modeDetailId AND g.datetime BETWEEN :datetimeFrom AND :datetimeTo "
+//                         + "GROUP BY g.myDeckId")
+//         List<DeckDistribution> getDeckDistributionsByDatetime(@Param("modeId") Integer modeId,
+//                         @Param("modeDetailId") Integer modeDetailId, @Param("datetimeFrom") LocalDateTime datetimeFrom,
+//                         @Param("datetimeTo") LocalDateTime datetimeTo, Pageable pageable);
+
+//         // 上位デッキの対面勝率一覧情報の取得
+//         @Query(value = "select NEW com.mtaerohand.duelsupportspring.repository.GameRepository.WinningRateForDeck("
+//                         + "cast(g.winCount / g.totalCount as float) as winningRate, "
+//                         + "cast(g.winCount as int) as winCount, "
+//                         + "cast (g.totalCount - g.winCount as int) as loseCount) "
+//                         + "from "
+//                         + "(select "
+//                         + "(select "
+//                         + "count(g3.id) "
+//                         + "from "
+//                         + "Game g3 "
+//                         + "where "
+//                         + "g3.myDeckId = :myDeckId "
+//                         + "AND g3.opDeckId = :opDeckId "
+//                         + "AND g3.modeId = :modeId "
+//                         + "AND g3.modeDetailId = :modeDetailId "
+//                         + ") as totalCount, "
+//                         + "count(g2.id) as winCount "
+//                         + "from "
+//                         + "Game g2 "
+//                         + "where "
+//                         + "g2.myDeckId = :myDeckId "
+//                         + "AND g2.opDeckId = :opDeckId "
+//                         + "AND g2.result = 1 "
+//                         + "AND g2.modeId = :modeId "
+//                         + "AND g2.modeDetailId = :modeDetailId "
+//                         + ") as g")
+//         WinningRateForDeck getWinningRateByDeckIds(@Param("modeId") Integer modeId,
+//                         @Param("modeDetailId") Integer modeDetailId,
+//                         @Param("myDeckId") Integer myDeckId,
+//                         @Param("opDeckId") Integer opDeckId);
+
+//         // 上位デッキの対面勝率一覧情報の取得(時間指定あり)
+//         @Query(value = "select NEW com.mtaerohand.duelsupportspring.repository.GameRepository.WinningRateForDeck("
+//                         + "cast(g.winCount / g.totalCount as float) as winningRate, "
+//                         + "cast(g.winCount as int) as winCount, "
+//                         + "cast (g.totalCount - g.winCount as int) as loseCount) "
+//                         + "from "
+//                         + "(select "
+//                         + "(select "
+//                         + "count(g3.id) "
+//                         + "from "
+//                         + "Game g3 "
+//                         + "where "
+//                         + "g3.myDeckId = :myDeckId "
+//                         + "AND g3.opDeckId = :opDeckId "
+//                         + "AND g3.modeId = :modeId "
+//                         + "AND g3.modeDetailId = :modeDetailId "
+//                         + "and g3.datetime between :datetimeFrom and :datetimeTo "
+//                         + ") as totalCount, "
+//                         + "count(g2.id) as winCount "
+//                         + "from "
+//                         + "Game g2 "
+//                         + "where "
+//                         + "g2.myDeckId = :myDeckId "
+//                         + "AND g2.opDeckId = :opDeckId "
+//                         + "AND g2.result = 1 "
+//                         + "AND g2.modeId = :modeId "
+//                         + "AND g2.modeDetailId = :modeDetailId "
+//                         + "and g2.datetime between :datetimeFrom and :datetimeTo "
+//                         + ") as g")
+//         WinningRateForDeck getWinningRateByDeckIdsByDatetime(@Param("modeId") Integer modeId,
+//                         @Param("modeDetailId") Integer modeDetailId, @Param("myDeckId") Integer myDeckId,
+//                         @Param("opDeckId") Integer opDeckId, @Param("datetimeFrom") LocalDateTime datetimeFrom,
+//                         @Param("datetimeTo") LocalDateTime datetimeTo);
+
+//         /**
+//          * ユーザIDに紐作れコードの一括削除
+//          *
+//          * @param userId
+//          */
+//         void deleteByUserId(String userId);
+// }
+
 /**
- * 試合情報リポジトリ
+ * 試合情報リポジトリ(本番用)
  */
 @Repository
 public interface GameRepository extends JpaRepository<Game, Integer>, JpaSpecificationExecutor<Game> {
         // デッキ分布一覧情報の取得
-        @Query("SELECT NEW com.mtaerohand.duelsupportspring.repository.GameRepository.DeckDistribution(g.myDeckId AS deckId, CAST(COUNT(g) / (SELECT COUNT(g2) FROM Game g2) AS FLOAT) AS ratio) "
+        @Query("SELECT NEW com.mtaerohand.duelsupportspring.repository.GameRepository.DeckDistribution(g.myDeckId AS deckId, COUNT(g) / CAST((SELECT COUNT(g2) FROM Game g2) AS FLOAT) AS ratio) "
                         + "FROM Game g WHERE g.modeId = :modeId AND g.modeDetailId = :modeDetailId "
                         + "GROUP BY g.myDeckId")
         List<DeckDistribution> getDeckDistributions(@Param("modeId") Integer modeId,
                         @Param("modeDetailId") Integer modeDetailId, Pageable pageable);
 
         // デッキ分布一覧情報の取得(時間指定あり)
-        @Query("SELECT NEW com.mtaerohand.duelsupportspring.repository.GameRepository.DeckDistribution(g.myDeckId AS deckId, CAST(COUNT(g) / (SELECT COUNT(g2) FROM Game g2) AS FLOAT) AS ratio) "
+        @Query("SELECT NEW com.mtaerohand.duelsupportspring.repository.GameRepository.DeckDistribution(g.myDeckId AS deckId, COUNT(g) / CAST((SELECT COUNT(g2) FROM Game g2) AS FLOAT) AS ratio) "
                         + "FROM Game g WHERE g.modeId = :modeId AND g.modeDetailId = :modeDetailId AND g.datetime BETWEEN :datetimeFrom AND :datetimeTo "
                         + "GROUP BY g.myDeckId")
         List<DeckDistribution> getDeckDistributionsByDatetime(@Param("modeId") Integer modeId,
@@ -32,7 +126,7 @@ public interface GameRepository extends JpaRepository<Game, Integer>, JpaSpecifi
 
         // 上位デッキの対面勝率一覧情報の取得
         @Query(value = "select NEW com.mtaerohand.duelsupportspring.repository.GameRepository.WinningRateForDeck("
-                        + "cast(g.winCount / g.totalCount as float) as winningRate, "
+                        + "g.winCount / cast(g.totalCount as float) as winningRate, "
                         + "cast(g.winCount as int) as winCount, "
                         + "cast (g.totalCount - g.winCount as int) as loseCount) "
                         + "from "
@@ -64,7 +158,7 @@ public interface GameRepository extends JpaRepository<Game, Integer>, JpaSpecifi
 
         // 上位デッキの対面勝率一覧情報の取得(時間指定あり)
         @Query(value = "select NEW com.mtaerohand.duelsupportspring.repository.GameRepository.WinningRateForDeck("
-                        + "cast(g.winCount / g.totalCount as float) as winningRate, "
+                        + "g.winCount / cast(g.totalCount as float) as winningRate, "
                         + "cast(g.winCount as int) as winCount, "
                         + "cast (g.totalCount - g.winCount as int) as loseCount) "
                         + "from "
